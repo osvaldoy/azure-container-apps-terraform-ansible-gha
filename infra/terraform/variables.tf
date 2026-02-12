3913233fe758588ffe9b919646b69442bf5a67c0
Dev User <dev@example.local>
2026-02-12 12:27:24 -0500

    refactor(terraform): add var.tags

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -3,5 +3,8 @@ variable "name" { type = string }
 variable "image_repository" { type = string }  # ex: "app"
 variable "image_tag" { type = string }         # ex: "dev" or sha
 
-
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
 
