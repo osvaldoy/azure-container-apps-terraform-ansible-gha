9c116740035ea8be999f5ace24cbb6ae25513615
Dev User <dev@example.local>
2026-02-12 12:20:56 -0500

    refactor(terraform): replace container_image with image_repository and image_tag

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -3,12 +3,5 @@ variable "name" { type = string }
 variable "image_repository" { type = string }  # ex: "app"
 variable "image_tag" { type = string }         # ex: "dev" or sha
 
-variable "container_image" {
-  description = "Full image ref, e.g. <acr>.azurecr.io/app:tag"
-  type        = string
-}
 
-variable "tags" {
-  type    = map(string)
-  default = {}
-}
+
