e991e49efbbb213a31962c3eec39bd73c3e3fac1
Dev User <dev@example.local>
2026-02-12 19:07:08 -0500

    fix(terraform): output full container app URL

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -3,5 +3,9 @@ output "acr_login_server" {
 }
 
 output "container_app_url" {
+  value = "https://${azurerm_container_app.app.ingress[0].fqdn}"
+}
+
+output "container_app_fqdn" {
   value = azurerm_container_app.app.ingress[0].fqdn
 }
